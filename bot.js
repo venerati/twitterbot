214a26458321bfd226ca74d88d32f306ccd288ed
James Marshall <jamesomarshall@gmail.com>
2016-12-03 17:12:27 -0500

    set up new requirement for the twitter account info. This is an external file set twitterconfig. this is not part of the git repo.

--- bot.js
+++ bot.js
@@ -2,11 +2,7 @@ console.log('bot.js has started');
 
 var Twit = require('twit');
 
+var Tconfig = require('./twitterconfig');
+
 //object that handles the twitter account info.
-var T = new Twit({
-  consumer_key:         '...',
-  consumer_secret:      '...',
-  access_token:         '...',
-  access_token_secret:  '...',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-})
+var T = new Twit(Tconfig);
