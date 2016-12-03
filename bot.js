5b4e21758df35d202bcfeb56aecf5f40991922d1
James Marshall <jamesomarshall@gmail.com>
2016-12-03 16:54:26 -0500

    created the DIR, set up the pacage.json file, set bot.js as the default page. inserted twit code for hooking twitter account to this app.

---
+++ bot.js
@@ -0,0 +1,12 @@
+console.log('bot.js has started');
+
+var Twit = require('twit');
+
+//object that handles the twitter account info.
+var T = new Twit({
+  consumer_key:         '...',
+  consumer_secret:      '...',
+  access_token:         '...',
+  access_token_secret:  '...',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+})
