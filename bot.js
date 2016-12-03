6c5bc9473eb5990f1c65b6a7dc78da83e6e33885
James Marshall <jamesomarshall@gmail.com>
2016-12-03 17:48:10 -0500

    set up parameters that twitter is asked to provide the latest 2 tweets that have #space in them. At this point I am console logging them.

--- bot.js
+++ bot.js
@@ -1,8 +1,19 @@
 console.log('bot.js has started');
 
 var Twit = require('twit');
-
+//this references a twitterconfig.js that has all the auth keys needed to connnect to twitter.
+//this is kept seperate for security reasons.
 var Tconfig = require('./twitterconfig');
 
 //object that handles the twitter account info.
 var T = new Twit(Tconfig);
+
+//this request the information from twitter. it is set to pull 2 things with 'space'
+T.get('search/tweets', { q: '#space', count: 2 }, function(err, data, response) {
+	//sets tweets to hold the data returned from the request
+	var tweets = data.statuses;
+	//goes through each search result one by one.
+	for (var i = 0; i < tweets.length; i++) {
+		console.log(tweets[i].text);
+	}
+});
