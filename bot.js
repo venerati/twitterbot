d27076b65366dae7ee4122c819b074a891603129
James Marshall <jamesomarshall@gmail.com>
2016-12-04 14:03:20 -0500

    bot now replies without getting stuck in a loop if @celifane is mentioned. Removed the search for space function from the top of the page.

--- bot.js
+++ bot.js
@@ -9,17 +9,6 @@ var Tconfig = require('./twitterconfig');
 //object that handles the twitter account info.
 var T = new Twit(Tconfig);
 
-//this request the information from twitter. it is set to pull 2 things with 'space'
-T.get('search/tweets', { q: '#space', count: 2 }, function(err, data, response) {
-	//sets tweets to hold the data returned from the request
-	var tweets = data.statuses;
-	//goes through each search result one by one.
-	for (var i = 0; i < tweets.length; i++) {
-		console.log(tweets[i].text);
-	}
-});
-
-
 //stream is set up to look at the bot's user account
 var stream = T.stream('user');
 
@@ -31,36 +20,36 @@ stream.on('tweet', tweetEvent);
 function tweetEvent(eventMsg) {
 
 	var replyTo = eventMsg.in_reply_to_screen_name;
-	console.log(replyTo);
 	var text = eventMsg.text;
-	console.log(text);
 	var from = eventMsg.user.screen_name;
-	console.log(from);
-	
+
+	//this var holds what the program will tweet to the account
+	var tweet = {
+		status:'I see ' + from + ' wishes to speak to the bot'
+	};
+
+	//if the stream sees that some one tweeted @celifane then it triggers the postTweet function
 	if (text.includes('@celifane')) {
 		postTweet();
 	} else {
-		console.log('post not triggered');
-	}
-};
+		console.log('postTweet not triggered');
+	};
 
-//this var holds what the program will tweet to the account
-var tweet = {
-	status: 'I see some one tweeted me'
-};
+	function postTweet () {
 
-function postTweet () {
+		//this is the actual  action of posting (action,variable being acted upon, function being called)
+		T.post('statuses/update', tweet, tweeted); 
 
-	//this is the actual  action of posting (action,variable being acted upon, function being called)
-	T.post('statuses/update', tweet, tweeted); 
-
-	function tweeted(err, data, response){
-  		if (err) {
-  		console.log('it is broken');
-  		} else {
-  		console.log('it worked');
-  		}
+		function tweeted(err, data, response){
+  			if (err) {
+  			console.log(err);
+  			} else {
+  			console.log('postTweet worked');
+  			}
+		};
 	};
 };
+
+
 
 
