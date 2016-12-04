53f08a7ef3b6b32c46c82001eeb3ef3afc1f771a
James Marshall <jamesomarshall@gmail.com>
2016-12-04 13:17:53 -0500

    at thsi point a stream as been established that checks any incoming and outgoing tweets from the user account.

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 console.log('bot.js has started');
 
 var Twit = require('twit');
+
 //this references a twitterconfig.js that has all the auth keys needed to connnect to twitter.
 //this is kept seperate for security reasons.
 var Tconfig = require('./twitterconfig');
@@ -18,17 +19,43 @@ T.get('search/tweets', { q: '#space', count: 2 }, function(err, data, response) 
 	}
 });
 
-var tweet = {
-	status: 'code test 2'
+
+//stream is set up to look at the bot's user account
+var stream = T.stream('user');
+
+//anytime some one tweets at the bot it will trigger tweetEvent
+stream.on('tweet', tweetEvent);
+
+
+//this function dictates what happens when some one tweets the bot
+function tweetEvent(eventMsg) {
+
+	var replyTo = eventMsg.in_reply_to_screen_name;
+	console.log(replyTo);
+	var text = eventMsg.text;
+	console.log(text);
+	var from = eventMsg.user.screen_name;
+	console.log(from);
+	
 };
 
-T.post('statuses/update', tweet, tweeted); 
+//this var holds what the program will tweet to the account
+var tweet = {
+	status: ''
+};
 
-function tweeted(err, data, response){
-  if (err) {
-  	console.log('it is broken');
-  } else {
-  	console.log('it worked');
-  }
-}
+function postTweet () {
+
+	//this is the actual  action of posting (action,variable being acted upon, function being called)
+	T.post('statuses/update', tweet, tweeted); 
+
+	function tweeted(err, data, response){
+  		if (err) {
+  		console.log('it is broken');
+  		} else {
+  		console.log('it worked');
+  		}
+	};
+};
+
 
